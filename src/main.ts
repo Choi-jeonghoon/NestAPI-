86886cc45904e84a00081aa31ca35dd8a5edbb7b
Choi-jeonghoon <jeongssi94@gmail.com>
2024-10-16 10:48:25 +0900

    Add: class Transformer 적용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //DTO에서 정의되지 않은 필드를 자동으로 제거하는 역할
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 필드가 있으면 요청을 거부하고 에러 발생
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,8 +1,8 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateMovieDto {
-  @IsNotEmpty()
-  @IsOptional()
+  @IsNotEmpty() // 값이 비어 있으면 안 됨 (빈 문자열 또는 null 허용 안 됨)
+  @IsOptional() // 해당 필드는 선택 사항임 (즉, 필수 입력이 아님)
   title?: string;
   @IsNotEmpty()
   @IsOptional()
